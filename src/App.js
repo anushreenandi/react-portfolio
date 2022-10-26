8272c29ab7a6b2403d51d835a90e795e19296e36
anushreenandi <110711445+anushreenandi@users.noreply.github.com>
2022-10-26 12:35:51 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/layout'
+import './App.scss'
+
+function App() {
+  return (
+  <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+          
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
